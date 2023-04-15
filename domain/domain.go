c7b800370d095ad4a481a4306b33aab150f81dee
Kambiz <kambiz.guity@gmail.com>
2023-04-15 12:22:20 +0330

    Four Chars

--- domain/domain.go
+++ domain/domain.go
@@ -2,23 +2,41 @@ package domain
 
 import (
 	"fmt"
-	"whois/alphabetic"
 )
 
-var Domains []string
-
-func makeDomain(content string) []string {
-	var domains []string
-	for _, v := range alphabetic.Alphabet {
-		var domain string = fmt.Sprint(content, v)
-		domain = fmt.Sprint("http://www.", domain, ".com")
-		domains = append(domains, domain)
-	}
-	return domains
+type Domain struct {
+	prefix  string
+	name    string
+	pastfix string
+	Status  int
+	domain  string
 }
 
-func AppendDomains(content string) {
-	var tempDomains = makeDomain(content)
-	fmt.Print(content, "\r")
-	Domains = append(Domains, tempDomains...)
+var Domains map[string]Domain = make(map[string]Domain)
+
+func (d *Domain) NewDomain(prefix string, name string, pastfix string) {
+	d.prefix = prefix
+	d.name = name
+	d.pastfix = pastfix
+	d.domain = fmt.Sprint(prefix, ".", name, ".", pastfix)
+	d.Status = 0
 }
+func (d Domain) GetKey() string {
+	return fmt.Sprint(d.name, ".", d.pastfix)
+}
+
+// func makeDomain(content string) []string {
+// 	var domains []string
+// 	for _, v := range alphabetic.Alphabet {
+// 		var domain string = fmt.Sprint(content, v)
+// 		domain = fmt.Sprint("http://www.", domain, ".com")
+// 		domains = append(domains, domain)
+// 	}
+// 	return domains
+// }
+
+// func AppendDomains(content string) {
+// 	var tempDomains = makeDomain(content)
+// 	fmt.Print(content, "\r")
+// 	Domains = append(Domains, tempDomains...)
+// }
--- main.go
+++ main.go
@@ -2,87 +2,95 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"sync"
 	"whois/alphabetic"
 	"whois/domain"
 	"whois/file"
 )
 
+type mutexDomain struct {
+	Domains map[string]domain.Domain
+	sync.Mutex
+}
+
 func main() {
+	var wg sync.WaitGroup
+	var domains mutexDomain
+
 	f := file.MakeCSVFile()
 	if f == nil {
 		fmt.Println("Error")
 		return
 	} else {
 		defer f.Close()
-		for _, valueA := range alphabetic.Alphabet {
-			for _, valueB := range alphabetic.Alphabet {
-				for _, valueC := range alphabetic.Alphabet {
-					tempString := fmt.Sprint(valueA, valueB, valueC)
-					domain.AppendDomains(tempString)
+		domains.Domains = make(map[string]domain.Domain)
+		wg.Add(1)
+		fmt.Println("Three chars")
+		go func() {
+			for _, valueA := range alphabetic.Alphabet {
+				for _, valueB := range alphabetic.Alphabet {
+					for _, valueC := range alphabetic.Alphabet {
+						tempString := fmt.Sprint(valueA, valueB, valueC)
+						addDomain(&domains, tempString)
+					}
 				}
 			}
+			wg.Done()
+		}()
+		fmt.Println("Four chars")
+		wg.Add(1)
+		go func() {
+			for _, valueA := range alphabetic.Alphabet {
+				for _, valueB := range alphabetic.Alphabet {
+					for _, valueC := range alphabetic.Alphabet {
+						for _, valueD := range alphabetic.Alphabet {
+							tempString := fmt.Sprint(valueA, valueB, valueC, valueD)
+							addDomain(&domains, tempString)
+						}
+					}
+				}
+			}
+			wg.Done()
+		}()
+		wg.Wait()
+		fmt.Println("Coping...")
+		domain.Domains = domains.Domains
+		fmt.Printf("len(domain.Domains): %v\n", len(domain.Domains))
+
+		keys := make([]string, 0, len(domain.Domains))
+		for k := range domain.Domains {
+			keys = append(keys, k)
 		}
-		file.AppendIntoFile(f)
 
-		// for _, valueA := range alphabetic.Alphabet {
-		// 	for _, valueB := range alphabetic.Alphabet {
-		// 		for _, valueC := range alphabetic.Alphabet {
-		// 			for _, valueD := range alphabetic.Alphabet {
-
-		// 				tempString := fmt.Sprint(valueA, valueB, valueC, valueD)
-		// 				domain.AppendDomains(tempString)
-
-		// 			}
-		// 		}
-		// 	}
-		// }
-		// file.AppendIntoFile(f)
-
-		// for _, valueA := range alphabetic.Alphabet {
-		// 	for _, valueB := range alphabetic.Alphabet {
-		// 		for _, valueC := range alphabetic.Alphabet {
-		// 			for _, valueD := range alphabetic.Alphabet {
-		// 				for _, valueE := range alphabetic.Alphabet {
-		// 					tempString := fmt.Sprint(valueA, valueB, valueC, valueD, valueE)
-		// 					domain.AppendDomains(tempString)
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
-
-		// for _, valueA := range alphabetic.Alphabet {
-		// 	for _, valueB := range alphabetic.Alphabet {
-		// 		for _, valueC := range alphabetic.Alphabet {
-		// 			for _, valueD := range alphabetic.Alphabet {
-		// 				for _, valueE := range alphabetic.Alphabet {
-		// 					for _, valueF := range alphabetic.Alphabet {
-		// 						tempString := fmt.Sprint(valueA, valueB, valueC, valueD, valueE, valueF)
-		// 						domain.AppendDomains(tempString)
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
-
-		// for _, valueA := range alphabetic.Alphabet {
-		// 	for _, valueB := range alphabetic.Alphabet {
-		// 		for _, valueC := range alphabetic.Alphabet {
-		// 			for _, valueD := range alphabetic.Alphabet {
-		// 				for _, valueE := range alphabetic.Alphabet {
-		// 					for _, valueF := range alphabetic.Alphabet {
-		// 						for _, valueG := range alphabetic.Alphabet {
-		// 							tempString := fmt.Sprint(valueA, valueB, valueC, valueD, valueE, valueF, valueG)
-		// 							domain.AppendDomains(tempString)
-		// 						}
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
+		sort.Strings(keys)
+		fmt.Printf("len(keys): %v\n", len(keys))
+		fmt.Println(keys[len(keys)-1])
+		fmt.Printf("keys[0]: %v\n", keys[0])
 
 	}
-
 }
+func addDomain(to *mutexDomain, with string) {
+	to.Lock()
+	var newDomain domain.Domain
+	newDomain.NewDomain("www", with, "com")
+	to.Domains[newDomain.GetKey()] = newDomain
+	to.Unlock()
+}
+
+// for _, valueA := range alphabetic.Alphabet {
+// 	for _, valueB := range alphabetic.Alphabet {
+// 		for _, valueC := range alphabetic.Alphabet {
+// 			for _, valueD := range alphabetic.Alphabet {
+// 				for _, valueE := range alphabetic.Alphabet {
+// 					for _, valueF := range alphabetic.Alphabet {
+// 						for _, valueG := range alphabetic.Alphabet {
+// 							tempString := fmt.Sprint(valueA, valueB, valueC, valueD, valueE, valueF, valueG)
+// 							domain.AppendDomains(tempString)
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
